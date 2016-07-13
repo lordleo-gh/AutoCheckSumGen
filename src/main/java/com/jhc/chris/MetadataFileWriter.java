d74888e9132625c59538c85da30f216c1c573ca4
Lord Leo <lordleo@web.de>
2016-07-13 16:32:10 +0100

    Just to develop on diff machine

--- src/main/java/com/jhc/chris/MetadataFileWriter.java
+++ src/main/java/com/jhc/chris/MetadataFileWriter.java
@@ -17,30 +17,9 @@ public class MetadataFileWriter implements Runnable {
     private final ChangedFile file;
     private final UUID uuid = UUID.randomUUID();
 
-//    public void setChangedFile(ChangedFile changedFile){
-//        this.changedFile = changedFile;
-//    }
-//    public synchronized ChangedFile getChangedFile(){
-//        return changedFile;
-//    }
-
     public MetadataFileWriter(ChangedFile changedFile){
         this.file = changedFile;
-       // uuid = UUID.randomUUID();
     }
-    // --- Start Singleton Pattern to have only one Filewriter instance dealing with all
-//    private static MetadataFileWriter instance = new MetadataFileWriter();
- //   private ChangedFileLog fileChangesLog = new ChangedFileLog();
-
-//    protected MetadataFileWriter() {
-//        // Exists only to defeat instantiation.
-//    }
-//    public static MetadataFileWriter getInstance() {
-//        return instance;
-//    }
-    // --- End Singleton Pattern
-
-   // private ChangedFileLog fileChangesLog;
 
     public void run() {
 
@@ -48,103 +27,153 @@ public class MetadataFileWriter implements Runnable {
             if (file.kind != ENTRY_DELETE){
                 CreateMetaFile();
             } else {
-                DeleteMetaFile();
+                deleteMetaFile();
             }
-            ClearUp();
+            removeThreadEntryFromThreadList();
         }
-
-
     }
 
     private void CreateMetaFile() {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         String md5HashValue = null;
         Boolean Success;
         do {
             Success = true;
             try {
-            //    System.out.println("Sleep");
                 Thread.sleep(1000);
             } catch (InterruptedException ix) {
                 Thread.currentThread().interrupt();
             }
-            try {
-                MessageDigest md = MessageDigest.getInstance("MD5");
 
-                FileInputStream fis = new FileInputStream(file);
+            md5HashValue = createMD5Hash();
 
-                byte[] dataBytes = new byte[1024];
+            if (md5HashValue != null) {
+//            try {
+//                MessageDigest md = MessageDigest.getInstance("MD5");
+//
+//                FileInputStream fis = new FileInputStream(file);
+//
+//                byte[] dataBytes = new byte[1024];
+//
+//                int nread = 0;
+//                while ((nread = fis.read(dataBytes)) != -1) {
+//                    md.update(dataBytes, 0, nread);
+//                }
+//                fis.close();
+//                byte[] mdbytes = md.digest();
+//                StringBuffer sb = new StringBuffer();
+//                for (int i = 0; i < mdbytes.length; i++) {
+//                    sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+//                }
+//                md5HashValue = sb.toString();
+//                System.out.println(file.getAbsolutePath() + " md5 Hash is " + md5HashValue + " Modified Date is " + sdf.format(file.lastModified()));
+//            } catch (NoSuchAlgorithmException e) {
+//                System.out.println("No such algorythm");
+//                Success = false;
+//            } catch (FileNotFoundException e) {
+//                System.out.println(e.toString());
+//                Success = false;
+//            } catch (IOException e) {
+//                System.out.println("IO exception");
+//                Success = false;
+//            } catch (Exception e) {
+//                System.out.println("Unknown error");
+//                Success = false;
+//            }
+                Success = createJHCStyleMetadataFile(md5HashValue);
+            } else { Success = false; }
+//            Wini ini = null;
+//            FileOutputStream output = null;
 
-                int nread = 0;
-                while ((nread = fis.read(dataBytes)) != -1) {
-                    md.update(dataBytes, 0, nread);
-                }
-                fis.close();
-                byte[] mdbytes = md.digest();
-                StringBuffer sb = new StringBuffer();
-                for (int i = 0; i < mdbytes.length; i++) {
-                    sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
-                }
-                md5HashValue = sb.toString();
-                System.out.println(file.getAbsolutePath() + " md5 Hash is " + md5HashValue + " Modified Date is " + sdf.format(file.lastModified()));
-            } catch (NoSuchAlgorithmException e) {
-                System.out.println("No such algorythm");
-                Success = false;
-            } catch (FileNotFoundException e) {
-                System.out.println(e.toString());
-                Success = false;
-            } catch (IOException e) {
-                System.out.println("IO exception");
-                Success = false;
-            } catch (Exception e) {
-                System.out.println("Unknown error");
-                Success = false;
-            }
-
-
-            // Properties prop = new Properties();
-            Wini ini = null;
-            FileOutputStream output = null;
-
-            try {
-                //ini = new Wini(new File(file.getPath() + ".metafile"));
-                ini = new Wini();
-                output = new FileOutputStream(file.getPath() + ".metadata");
-
-                ini.put(file.getName(), "FileMD5Signature", md5HashValue);
-                ini.put(file.getName(), "FileCreationDate", "01/01/1999");
-                ini.put(file.getName(), "FileModificationDate", sdf.format(file.lastModified()));
-                ini.put(file.getName(), "FileSize", String.valueOf(file.length()));
-                ini.put(file.getName(), "FileVersion", "0.0.0.0");
-
-                // save properties
-                ini.store(output);
-
-            } catch (IOException io) {
-                io.printStackTrace();
-                Success = false;
-            }
+//            try {
+//
+//                ini = new Wini();
+//                output = new FileOutputStream(file.getPath() + ".metadata");
+//                ini.put(file.getName(), "FileMD5Signature", md5HashValue);
+//                ini.put(file.getName(), "FileCreationDate", "01/01/1999");
+//                ini.put(file.getName(), "FileModificationDate", sdf.format(file.lastModified()));
+//                ini.put(file.getName(), "FileSize", String.valueOf(file.length()));
+//                ini.put(file.getName(), "FileVersion", "0.0.0.0");
+//
+//                // save properties
+//                ini.store(output);
+//
+//            } catch (IOException io) {
+//                io.printStackTrace();
+//                Success = false;
+//            }
         } while (!Success);
         System.out.format("%s: Ended %s\n", uuid, file.getAbsolutePath());
-//        ThreadList RunningThreadList = ThreadList.getInstance();
-//        for (Thread WriterThread: RunningThreadList.getRunningWriterThreads()){
-//            if (WriterThread.getName().equals(file.getAbsolutePath())){
-//                // Remove finished threads from list - causes ConcurrentModifcationException
-//                RunningThreadList.getRunningWriterThreads().remove(WriterThread);
-//            }
-//        }
     }
 
+    private String createMD5Hash() {
+        String md5HashValue = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
 
-    private void DeleteMetaFile(){
+            FileInputStream fis = new FileInputStream(file);
+
+            byte[] dataBytes = new byte[1024];
+
+            int nread = 0;
+            while ((nread = fis.read(dataBytes)) != -1) {
+                md.update(dataBytes, 0, nread);
+            }
+            fis.close();
+            byte[] mdbytes = md.digest();
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < mdbytes.length; i++) {
+                sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            md5HashValue = sb.toString();
+            //System.out.println(file.getAbsolutePath() + " md5 Hash is " + md5HashValue + " Modified Date is " + sdf.format(file.lastModified()));
+            return md5HashValue;
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("No such algorythm");
+            return null;
+        } catch (FileNotFoundException e) {
+            System.out.println(e.toString());
+            return null;
+        } catch (IOException e) {
+            System.out.println("IO exception");
+            return null;
+        } catch (Exception e) {
+            System.out.println("Unknown error");
+            return null;
+        }
+    }
+
+    private boolean createJHCStyleMetadataFile(String md5HashValue){
+      //  Wini ini = null;
+       // FileOutputStream output = null;
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            Wini ini = new Wini();
+            FileOutputStream output = null;
+            output = new FileOutputStream(file.getPath() + ".metadata");
+            ini.put(file.getName(), "FileMD5Signature", md5HashValue);
+            ini.put(file.getName(), "FileCreationDate", "01/01/1999");
+            ini.put(file.getName(), "FileModificationDate", sdf.format(file.lastModified()));
+            ini.put(file.getName(), "FileSize", String.valueOf(file.length()));
+            ini.put(file.getName(), "FileVersion", "0.0.0.0");
+
+            // save properties
+            ini.store(output);
+            return true;
+        } catch (IOException io) {
+            io.printStackTrace();
+            return false;
+        }
+    }
+
+    private void deleteMetaFile(){
         File metaFile = new File(file.getAbsolutePath() + ".metadata");
         if (metaFile.exists()){
             metaFile.delete();
         }
     }
 
-    private void ClearUp(){
+    private void removeThreadEntryFromThreadList(){
         System.out.format("%s Ended %s\n", uuid, file.getAbsolutePath());
         ThreadList RunningThreadList = ThreadList.getInstance();
         for (Thread WriterThread: RunningThreadList.getRunningWriterThreads()){
@@ -153,7 +182,6 @@ public class MetadataFileWriter implements Runnable {
                 RunningThreadList.getRunningWriterThreads().remove(WriterThread);
             }
         }
-        System.out.format("File %s", file.getAbsolutePath());
         System.out.println("----------------");
         for (Thread WriterThread: RunningThreadList.getRunningWriterThreads()){
             System.out.format("%s \n",WriterThread);
